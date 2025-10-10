ab225edb5670ec8ac44329bf483d946e0c9598d6
= <smi23136@byui.edu>
2025-10-10 16:14:46 -0600

    finished the coolpics part 2 assignment

---
+++ coolpics/coolpics.js
@@ -0,0 +1,48 @@
+const menuBtn = document.querySelector(".menu-button")
+const menuItems = document.querySelectorAll(".menu-item")
+const gallery = document.querySelector(".gallery")
+const modal = document.querySelector("dialog")
+const modalImage = document.querySelector(".modal-image")
+const closeBtn = document.querySelector(".close-viewer")
+
+function toggleMenu() {
+	menuItems.forEach((element) => {
+		element.classList.toggle("hidden")
+	})
+}
+
+menuBtn.addEventListener("click", toggleMenu)
+
+window.addEventListener("resize", handleResize)
+
+gallery.addEventListener("click", displayModal)
+
+function displayModal(event) {
+	image = event.target.closest("img")
+	modalImage.src = image.src.split("-")[0] + "-full.jpeg"
+	console.log(modalImage.src)
+	modalImage.alt = "Larger image"
+	modal.showModal()
+}
+
+closeBtn.addEventListener("click", () => {
+	modal.close()
+})
+
+modal.addEventListener("click", (event) => {
+	if (event.target === modal) {
+		modal.close()
+	}
+})
+
+function handleResize() {
+	if (window.innerWidth >= 1000) {
+		menuItems.forEach((element) => {
+			element.classList.remove("hidden")
+		})
+	} else {
+		menuItems.forEach((element) => {
+			element.classList.remove("hidden")
+		})
+	}
+}
